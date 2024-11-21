ccea4b8c0313980fee0f40d9e3200a67b3ce9672
bell <876662727@qq.com>
2024-11-22 01:45:55 +0800

    feat: 自定义eslint-plugin-bell插件

---
+++ packages/eslint-plugin-bell/configs/recommended.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: ['eslint-plugin-bell'],
+    rules: {
+      'eslint-plugin-bell/no-http-url': 'warn',
+      'eslint-plugin-bell/no-secret-info': 'error',
+    },
+  };
+  

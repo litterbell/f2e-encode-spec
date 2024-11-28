7f36b82b0918222b79c58b22f9ae53b47bb673e3
litterbell <876662727@qq.com>
2024-11-28 15:37:33 +0000

    Deploying to gh-pages from @ litterbell/f2e-encode-spec@6f3369f3e849f79fc02368f72acaf8f6392a8ea4 🚀

--- packages/eslint-plugin-bell/configs/recommended.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: ['eslint-plugin-bell'],
-    rules: {
-      'eslint-plugin-bell/no-http-url': 'warn',
-      'eslint-plugin-bell/no-secret-info': 'error',
-    },
-  };
-  

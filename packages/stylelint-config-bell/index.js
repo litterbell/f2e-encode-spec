61464ddf37a52bd0b519b03e6eac161c85e95558
bell <876662727@qq.com>
2025-03-03 08:24:52 +0800

    feat: 修改邮箱&clean script

--- packages/stylelint-config-bell/index.js
+++ packages/stylelint-config-bell/index.js
@@ -1,7 +1,7 @@
 /*
- * @Author: bell 876662727@qq.com
+ * @Author: bell trao2021@163.com
  * @Date: 2024-11-21 01:04:07
- * @LastEditors: bell 876662727@qq.com
+ * @LastEditors: bell trao2021@163.com
  * @LastEditTime: 2024-11-21 01:04:26
  * @FilePath: \GIT\f2e-encode-spec\packages\stylelint-config-bell\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
